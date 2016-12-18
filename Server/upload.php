a3e4473f9f80d14fe8900871fe80ee3cad11868d
Jean Lacassagne <jean.lacassagne@gmail.com>
2016-12-18 01:20:28 +0100

    Update upload.php
    This fix enables transfering binary files.

--- Server/upload.php
+++ Server/upload.php
@@ -1,7 +1,10 @@
 <?php
 if (isset($_GET['filename']) and isset($_GET['content'])) {
         $myfile = fopen('uploads/' . $_GET['filename'] .'-' .  date('Y-m-d-H-i-s'), 'a+');
-        fputs($myfile, $_GET['content']);
+        for ($i = 0; $i < strlen($_GET['content']); $i += 2)
+        {
+                fputs($myfile, chr(hexdec($_GET['content'][$i].$_GET['content'][$i + 1])));
+        }
         fclose($myfile);
 }
 ?>
